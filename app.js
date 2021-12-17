4898d23a0ffdb2d810b34bb49039a9ebe8202526
Nikhil <nkmnikhil0009@gmail.com>
2021-12-18 00:51:59 +0530

    add project section

---
+++ app.js
@@ -0,0 +1,12 @@
+let swiper = new Swiper(".projects-container", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+});
