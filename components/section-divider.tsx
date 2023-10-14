eaccfee52183e54aed7cf1ef06be0a4771102f8e
thehashmap <nkmnikhil0009@gmail.com>
2023-10-14 11:56:35 +0530

    feat: add section divider

---
+++ components/section-divider.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import React from "react";
+import { motion } from "framer-motion";
+
+export default function SectionDivider() {
+  return (
+    <motion.div
+      className="absolute w-full flex justify-center items-center"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.125 }}
+    >
+      <a href="#about">
+        <div className="w-[35px] h-[64px] rounded-3xl border-4 border-gray-200 flex justify-center items-start p-2">
+          <motion.div
+            animate={{
+              y: [0, 24, 0],
+            }}
+            transition={{
+              duration: 1.5,
+              repeat: Infinity,
+              repeatType: "loop",
+            }}
+            className="w-3 h-3 rounded-full bg-gray-200 mb-1"
+          />
+        </div>
+      </a>
+    </motion.div>
+  );
+}
