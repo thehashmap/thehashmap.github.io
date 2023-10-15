819126d9f0060153c87330ee2ab80b3b292a019f
thehashmap <nkmnikhil0009@gmail.com>
2023-10-15 18:32:06 +0530

    feat: add skills section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import About from "@/components/about";
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
+import Skills from "@/components/skills";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Intro />
       <SectionDivider />
       <About />
+      <Skills />
       <Projects />
     </main>
   );
