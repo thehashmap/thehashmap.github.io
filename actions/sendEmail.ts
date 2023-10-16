8c0ce9fd0c677f47ab3d63dab012884d5b8b99f5
thehashmap <nkmnikhil0009@gmail.com>
2023-10-16 19:04:57 +0530

    feat: add contact form

---
+++ actions/sendEmail.ts
@@ -0,0 +1,52 @@
+"use server";
+
+import React from "react";
+import { Resend } from "resend";
+import { validateString, getErrorMessage } from "@/lib/utils";
+import ContactFormEmail from "@/email/contact-form-email";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendEmail = async (formData: FormData) => {
+  const senderEmail = formData.get("senderEmail");
+  const message = formData.get("message");
+
+  console.log(senderEmail);
+  console.log(message);
+
+  // simple server-side validation
+  if (!validateString(senderEmail, 500)) {
+    return {
+      error: "Invalid sender email",
+    };
+  }
+  if (!validateString(message, 5000)) {
+    return {
+      error: "Invalid message",
+    };
+  }
+
+  let data;
+  try {
+    console.log("trying resend");
+    data = await resend.emails.send({
+      from: "Contact form <onboarding@resend.dev>",
+      to: ["nkmnikhil0009@gmail.com"],
+      subject: "Message from contact form",
+      reply_to: senderEmail,
+      react: React.createElement(ContactFormEmail, {
+        message: message,
+        senderEmail: senderEmail,
+      }),
+    });
+    console.log("data: ", data);
+  } catch (error: unknown) {
+    return {
+      error: getErrorMessage(error),
+    };
+  }
+
+  return {
+    data,
+  };
+};
---
+++ components/contact.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+import { sendEmail } from "@/actions/sendEmail";
+import SubmitBtn from "./submit-btn";
+import toast from "react-hot-toast";
+
+export default function Contact() {
+  const { ref } = useSectionInView("Contact");
+
+  return (
+    <motion.section
+      id="contact"
+      ref={ref}
+      className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1,
+      }}
+      viewport={{
+        once: true,
+      }}
+    >
+      <SectionHeading>Contact me</SectionHeading>
+
+      <p className="text-gray-700 -mt-6 ">
+        Please contact me directly at{" "}
+        <a className="underline" href="mailto:nikhil.kumar.cer19@iitbhu.ac.in">
+          nikhil.kumar.cer19@iitbhu.ac.in
+        </a>{" "}
+        or through this form.
+      </p>
+
+      <form
+        className="mt-10 flex flex-col "
+        action={async (formData) => {
+          //   console.log(formData);
+          const senderEmail = formData.get("senderEmail");
+          const message = formData.get("message");
+
+          console.log("client side: ", senderEmail);
+          console.log(message);
+          const { data, error } = await sendEmail(formData);
+
+          if (error) {
+            toast.error(error);
+            return;
+          }
+
+          toast.success("Email sent successfully!");
+        }}
+      >
+        <input
+          className="h-14 px-4 rounded-lg borderBlack "
+          name="senderEmail"
+          type="email"
+          required
+          maxLength={500}
+          placeholder="Your email"
+        />
+        <textarea
+          className="h-52 my-3 rounded-lg borderBlack p-4 "
+          name="message"
+          placeholder="Your message"
+          required
+          maxLength={5000}
+        />
+        <SubmitBtn />
+      </form>
+    </motion.section>
+  );
+}
