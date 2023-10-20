ab5b109ec4d405db84f11aec248939e342586bec
thehashmap <nkmnikhil0009@gmail.com>
2023-10-20 18:40:42 +0530

    fix: alignment

--- app/page.tsx
+++ app/page.tsx
@@ -9,15 +9,19 @@ import Skills from "@/components/skills";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center px-4">
-      <Intro />
-      <SectionDivider />
-      <About />
+    <main>
+      <div className="flex flex-col items-center px-4">
+        <Intro />
+        <SectionDivider />
+        <About />
+      </div>
       <Experience />
-      <Skills />
-      <Calendar />
-      <Projects />
-      <Contact />
+      <div className="flex flex-col items-center px-4">
+        <Skills />
+        <Calendar />
+        <Projects />
+        <Contact />
+      </div>
     </main>
   );
 }
